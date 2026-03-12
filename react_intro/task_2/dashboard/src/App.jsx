e4ab253c59f5241c39487796fbd1edf501ce81dd
denis-nikolli <denisnikolli@yahoo.com>
2026-03-12 18:44:43 +0100

    hiuguo

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import holbertonLogo from './assets/holberton-logo.jpg'
-import { getCurrentYear, getFooterCopy } from './utils'
-import Notifications from './Notifications'
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+import image3 from './assets/holberton-logo.jpg';
+import './App.css';
+
 
 function App() {
 
   return (
     <>
-      <div className='root-notifications'>
+      <div>
         <Notifications />
       </div>
-      <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo"></img>
-        <h1 style={{color: "#e1003c"}}>School Dashboard</h1>
+      <div className='App-header'>
+        <img src={image3} alt='holberton logo' />
+        <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+
+      <div className='App-body'>
+        <p> Login to access the full dashboard</p>
+        <label htmlFor="email">email</label>
+        <input type="email" id="email"></input>
+
+        <label htmlFor="password">password</label>
+        <input type="password" id="password"></input>
+
+        <button type="submit">OK</button>
       </div>
-      <div className="App-footer">
-        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      <div className='App-footer'>
+        <p>Copyright {getCurrentYear()} -  {getFooterCopy(true)}</p>
       </div>
     </>
   )
